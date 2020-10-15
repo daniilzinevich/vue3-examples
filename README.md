628877340fcf706387e1e307f84fd85066426c42
Daniil Zinevich <danila2412@gmail.com>
2020-10-15 18:17:58 +0300

    Use Vue 3 reactivity

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Vue 3 examples
 
-This is a simple vue application that uses reactive state and computed values  
+This is a simple vanila JS application that use Vue 3 reactivity sistem directly injected. It's bundle size is a lot smaller providing similar functionality  
 
 This project is a few proof of concepts for presentation over lesser known Vue features and their Vue 3 implementation  
 Presentation is here https://slides.com/daniilzinevich/vue3_tips
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { reactive, computed, watch } from '@vue/runtime-core';
 
-createApp(App).mount('#app')
+function useReactive() {
+  const toggles = reactive({
+    toggleA: false,
+    toggleB: true
+  });
+
+  const bothToggled = computed(() => toggles.toggleA && toggles.toggleB);
+
+  return [
+    toggles,
+    bothToggled,
+  ];
+}
+
+const [toggles, both] = useReactive();
+debugger;
+
+document.getElementById('toggle1').checked = toggles.toggleA;
+document.getElementById('toggle1').addEventListener('change', (event) => { toggles.toggleA = event.target.checked });
+document.getElementById('toggle2').checked = toggles.toggleB;
+document.getElementById('toggle2').addEventListener('change', (event) => { toggles.toggleB = event.target.checked });
+document.getElementById('both').value = both.value;
+watch(both, () => document.getElementById('both').value = both.value);
