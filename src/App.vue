a811786b60f04f80dbb348bdae688a1be7be4838
Daniil Zinevich <danila2412@gmail.com>
2020-10-15 18:13:28 +0300

    Use Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
   <div>
-    <input type="checkbox" v-model="toggleA">
+    <input type="checkbox" v-model="toggles.toggleA">
     Toggle A
   </div>
   <div>
-    <input type="checkbox" v-model="toggleB">
+    <input type="checkbox" v-model="toggles.toggleB">
     Toggle B
   </div>
   <input :value="bothToggled">
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      toggleA: false,
-      toggleB: true
-    };
-  },
-  computed: {
-    bothToggled() {
-      return this.toggleA && this.toggleB;
-    }
-  }
-};
+<script setup>
+import { reactive, computed } from '@vue/reactivity';
+
+export const toggles = reactive({
+  toggleA: false,
+  toggleB: true
+});
+
+export const bothToggled = computed(() => toggles.toggleA && toggles.toggleB);
+
 </script>
